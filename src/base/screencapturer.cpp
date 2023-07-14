eb76c17338de4ae77110d75b587341d0bc8c052c
LordAlmightyIsMySheperd <liqitong@163.com>
2023-07-14 14:27:06 +0800

    Removed old ScreenCapturer declaration

--- src/base/screencapturer.cpp
+++ src/base/screencapturer.cpp
@@ -1,29 +1,5 @@
-#include "screenshotter.h"
+#include "screencapturer.h"
 
 _START_SCREEN2WEB_NM_
-
-SDL_Surface *ScreenCapturer::GetWindowSurface(const std::string &windowName) noexcept
-{
-    SDL_Window *window = SDL_GetWindowFromID(SDL_GetWindowID(SDL_GetWindows()->id));
-    if (window == nullptr)
-    {
-        return nullptr;
-    }
-    return SDL_GetWindowSurface(window);
-}
-
-SDL_Window *ScreenCapturer::FindWindowByName(const ::std::string &windowName) noexcept
-{
-    SDL_Window *wnd = nullptr;
-
-    for (int i = 0; i < SDL_GetNumVideoDrivers(); i++)
-    {
-        ::std::string title = SDL_GetVideoDriver(i);
-        SDL_GetWindowTitle() if (title == windowName)
-        {
-            wnd =
-        }
-    }
-}
 
 _END_SCREEN2WEB_NM_
