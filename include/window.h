c0cbfe720f3ea70fcbde1030ae1dddbc5bf83808
GodAlmightyByMySide <liqitong@163.com>
2023-07-14 11:54:39 +0800

    [ Screen2Web ] Implemented basic imgui window class

---
+++ include/window.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "defines.h"
+
+#include <SDL.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL_opengles2.h>
+#else
+#include <SDL_opengl.h>
+#endif
+
+_START_SCREEN2WEB_NM_
+
+class Window
+{
+    SCREEN2WEB_SINGLETON(Window)
+
+public:
+    int Init() noexcept;
+
+    int Loop() noexcept;
+
+    int DeInit() noexcept;
+
+private:
+    bool done_{false};
+    SDL_Window *window_{nullptr};
+    SDL_GLContext gl_context_{nullptr};
+};
+
+_END_SCREEN2WEB_NM_
