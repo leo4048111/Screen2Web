19733eb105d311fa8cfa35a1f11ae1b3cc1c48ab
LordAlmightyIsMySheperd <liqitong@163.com>
2023-07-18 15:14:10 +0800

    [ Screen2Web/base ] Added submodule cpp-httplib and httpserver.cpp
    [ Screen2Web/base ] Added submodule cpp-httplib and httpserver.cpp

---
+++ include/httpserver.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "defines.h"
+
+_START_SCREEN2WEB_NM_
+
+class HttpServer
+{
+    SCREEN2WEB_SINGLETON(HttpServer)
+
+public:
+    
+};
+
+_END_SCREEN2WEB_NM_
---
+++ src/base/httpserver.cpp
@@ -0,0 +1,7 @@
+#include "httpserver.h"
+
+#include <httplib.h>
+
+_START_SCREEN2WEB_NM_
+
+_END_SCREEN2WEB_NM_
